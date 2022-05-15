b443c81e3533d66b2d8b435fc01f1706ee57a69d
Chris <chrisalexander0617@gmail.com>
2022-05-15 12:32:14 -0400

    Added frontend

--- backend/lib/agencyApi.js
+++ backend/lib/agencyApi.js
@@ -2,11 +2,25 @@
 require('dotenv').config()
 const axios = require('axios')
 
-const getAgency = (ORI) => {
+//https://api.usa.gov/crime/fbi/sapi/api/agencies/byStateAbbr/MI?API_KEY=NbUnDtUTfDjilbXWDFlcaitwa2t81Flp6F2OcHjy
+// Docs: https://crime-data-explorer.app.cloud.gov/pages/docApi
+
+
+/**
+Allows you to find police agencies by State, or ORI number listing basic details
+ **/
+
+const getAgencyByORI = ori => {
     const uri = `https://api.usa.gov/crime/fbi/sapi/api/agencies/MI1972500?API_KEY=${process.env.API_KEY}`
     try {const data = axios.get(uri); return data} catch {return false}
 }
 
+const getAgenciesByState = state => {
+    const uri = `https://api.usa.gov/crime/fbi/sapi/api/agencies/byStateAbbr/${state}?API_KEY=${process.env.API_KEY}`
+    try {const data = axios.get(uri); return data} catch {return false}
+}
+
 module.exports = {
-    getAgency:getAgency
+    getAgencyByORI:getAgencyByORI,
+    getAgenciesByState:getAgenciesByState
 }
